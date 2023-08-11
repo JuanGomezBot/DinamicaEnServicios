27d0dc1524740847047cc53cb04e14f40d0f1a0e
Your Name <you@example.com>
2023-08-11 13:12:15 -0500

    Progreso dinamica en servicios

---
+++ Squadfree/assets/js/segchanger.js
@@ -0,0 +1,31 @@
+function changeCSS(cssFile, cssLinkIndex) {
+    // Get the button element by its ID
+const boton = document.getElementById('boton');
+const boton2 = document.getElementById('boton2')
+    
+var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+var newlink = document.createElement("link");
+newlink.setAttribute("rel", "stylesheet");
+newlink.setAttribute("type", "text/css");
+newlink.setAttribute("href", cssFile);
+
+document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
+
+}
+    
+
+function enableStylesheet () {
+    
+    changeCSS("assets/css/seg.css",'css')
+    boton2.style.backgroundColor = '#17A63B';
+    boton.style.backgroundColor = '#F2F2F2';
+
+  }
+  
+  function colorblind () {changeCSS("assets/css/invertseg.css",'css');
+    boton.style.backgroundColor = '#17A63B';
+    boton2.style.backgroundColor = '#F2F2F2';
+    
+
+  }
